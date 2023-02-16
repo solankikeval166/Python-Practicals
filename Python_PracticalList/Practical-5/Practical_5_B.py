81e5d3824924066e8c3899142e7d2b966442b954
solankikeval166 <solankikeval166@gmail.com>
2023-02-16 13:54:39 +0530

    Practical 5 Completed

---
+++ Python_PracticalList/Practical-5/Practical_5_B.py
@@ -0,0 +1,25 @@
+#declaring class Employee
+class Employee:
+    
+#initializes constructor 
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+#display function for display information
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Salary:", self.salary, end="\n\n")
+
+#updatedSalary function will calculate increased salary 
+    def updatedSalary(self):
+        self.salary = self.salary * 1.04
+        print("Updated Salary ", self.salary)
+
+#creating a emp object 
+emp = Employee("Keval", 18, 1000)
+
+emp.updatedSalary()
+
